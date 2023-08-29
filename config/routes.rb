212d4588043e36deb64e3aba601098bd33ab2989
shaswat-satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-08-29 07:07:37 +0530

    Revert "Added Comments"
    This reverts commit 7340d99dafcfb5cfa8cda5bb0f4caf6e732fb343.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-  resources :articles do 
-    resources :comments
-  end
+  resources :articles
 
-  
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
